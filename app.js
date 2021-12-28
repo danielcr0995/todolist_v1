25ab68e623fbe22bc55ffa621125d4b34723fbdd
danielcr0995 <daniel.correa.1995@gmail.com>
2021-12-28 16:04:57 -0500

    tells the day of the week

--- app.js
+++ app.js
@@ -9,12 +9,36 @@ app.get('/', function(req,res){
     var today=new Date();
     var currentDay= today.getDay();
     var day='';
-    if (currentDay===6 || currentDay===0){
-        day='Weekend'
-    }else{
-        day='Weekday'
+
+    switch (currentDay){
+        case 0:
+            day='Sunday';
+            break;
+        case 1:
+            day='Monday';
+            break;
+        case 2:
+            day='Tuesday';
+            break;
+        case 3:
+            day='Wednesday';
+            break;
+        case 4:
+            day='Thursday';
+            break;
+        case 5:
+            day='Friday';
+            break;
+        case 6:
+            day='Saturday';
+            break;
     }
-    res.render('list', {kindOfDay:day})
+    // if (currentDay===6 || currentDay===0){
+    //     day='Weekend'
+    // }else{
+    //     day='Weekday'
+    // }
+    res.render('list', {kindOfDay:day});//KindOfDay goes in ejs file, day is the variale in this file
 })
 
 app.listen(3000, function(){
